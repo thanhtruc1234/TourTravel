a9a8fda63d6ded837a0b3de13e87edeb83aabfbd
thanhtruc1234 <91402094+thanhtruc1234@users.noreply.github.com>
2021-10-28 20:12:30 +0700

    Add files via upload

---
+++ introduceVietnam.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welecom to VietNam</title>
+        <link rel="icon" href="./upload/logodulichht.png">
+        <link rel="stylesheet" href="./include/fontawesome/css/all.css">
+        <link rel="stylesheet" href="./include/style/bootstrap.css">
+        <link rel="stylesheet" href="include/mystyle45646.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <?php session_start(); ?>
+        <style>
+            body{
+                background-image: url('../upload/hinh-anh-du-lich-ha-long.jpg');
+            }
+        </style>
+    </head>
+    <body>
+    <?php require_once "./view/header.html" ?>
+
+
+   <div class="article-composition" style="">
+        <div class="container">
+            <p style="color:#000;font-family: 'ZCOOL QingKe HuangYou', cursive; font-weight:500; font-size:38px; ">Vẻ đẹp độc đáo của đất nước Việt Nam</p>
+            <p>Chúng ta tự hào về một đất nước Việt Nam - Đất nước có 54 dân tộc anh em, với những vùng đất có bề dày lịch sử hàng nghìn năm. Một đất nước hình chữ S bên bờ biển Đông, một đất nước với ba miền Bắc - Trung - Nam, cũng là ba miền văn hóa khác nhau, nhưng đan xen lẫn nhau, vừa riêng biệt, vừa có những nét chung, tạo thành một thể thống nhất. Cũng vì thế mà đất nước Việt Nam mang một vẻ đẹp riêng biệt, một vẻ đẹp Việt Nam.
+
+                Vẻ đẹp của Việt Nam được thể hiện trong từng bức ảnh, từng họa tiết, từng chi tiết của cuộc sống và con người Việt Nam nơi đây. Với những nét đẹp riêng của từng vùng.</p>
+            <img style="width: 900px;" src="upload/gt1.jpg"> <br>
+            <p>…. Tomb statues of ethnic minorities in the Central Highlands are very beautiful, rustic and simple.</p><br>
+            <img style="width: 500px;" src="upload/gt2.jpg"><br>
+            <p>…Sculpture of the Cham is a beauty of sophistication, sophistication, carefulness and balance.</p><br>
+            <img style="width: 900px;" src="upload/gt3.jpg"><br>
+            <p>… to the image of the unique One Pillar Pagoda of Vietnam</p><br>
+            <img style="width: 900px;" src="upload/gt4.jpg"><br>
+            <p>The beauty of the Northwest</p><br>
+            <img style="width: 900px;" src="upload/gt5.jpg"><br><br>
+            <img style="width: 900px;" src="upload/gt6.jpg"><br>
+            <p>The characteristics of the capital Hanoi</p><br>
+            <img style="width: 900px;" src="upload/gt7.jpg"><br>
+            <p>Halong Bay with majestic beautiful scenes</p><br>
+            <img style="width: 900px;" src="upload/gt8.jpg"><br>
+            <p>The romantic white sand strip of Nha Trang beach</p> <br><br>
+            <p>With those unique beauties, Vietnam has created a lively country, contributing to adorn the beauty of our beloved Earth.ads. Admission information for Kindergarten at Hanoi Pedagogical College.</p>
+         </div>
+   </div>
+
+
+   <?php require_once "./view/footer.php"; ?>
+   
+</body>
+</html>
